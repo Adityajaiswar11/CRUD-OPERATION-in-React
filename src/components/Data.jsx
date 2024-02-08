5e0c610959becdb83df980fcf68c1898a3ab50ca
Aditya Jaiswar <adityajaiswar11@gmail.com>
2024-02-07 20:40:24 -0800

    All done

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,21 +1,18 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 
 const Data = () => {
   //getting data from local storage
   const getData = () => {
     let data = localStorage.getItem("data");
-    if (data == []) {
-      return [];
-    } else {
-      return JSON.parse(data);
-    }
+    return data ? JSON.parse(data) : [];
   };
 
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [task, setTask] = useState("");
+  const [editid, setEditid] = useState(0);
   const [inputValues, setInputvalues] = useState(getData());
 
   //storing data in local storage
@@ -27,6 +24,23 @@ const Data = () => {
   //   submit inputdata
   const submitHandler = (e) => {
     e.preventDefault();
+
+    //code  for edit data
+    if (editid) {
+      const editdata = inputValues.find((d) => d.id === editid);
+      const editedData = inputValues.map((data) =>
+        data.id === editdata.id
+          ? (data = { id: data.id, name, email, task })
+          : { id: data.id, name: data.name, email: data.email, task: data.task }
+      );
+      setInputvalues(editedData);
+      setName("");
+      setEmail("");
+      setTask("");
+      setEditid(0);
+      return;
+    }
+
     if (!name || !email || !task) {
       toast.error("Please fill out all fields!", {
         position: "top-center",
@@ -34,28 +48,31 @@ const Data = () => {
     }
 
     if (name !== "" && email !== "" && task !== "") {
-      setInputvalues([...inputValues, { email, name, task }]);
+      setInputvalues([
+        { id: `${name}-${Date.now()}`, name, email, task },
+        ...inputValues,
+      ]);
+      setName("");
       setEmail("");
       setTask("");
-      setName("");
     }
   };
 
   //   function for edit data
 
-  const handleEdit = (value, id) => {
-    setEmail(value.email);
-    setName(value.name);
-    setTask(value.task);
-    let newData = inputValues.filter((data, i) => i !== id);
-    setInputvalues(newData);
+  const handleEdit = (value) => {
+    let newData = inputValues.find((data) => data.id === value.id);
+    setName(newData.name);
+    setEmail(newData.email);
+    setTask(newData.task);
+    setEditid(value.id);
   };
 
   //function for delete data from table
 
   const handleDelete = (id) => {
-    let newData = inputValues.filter((data, i) => i !== id);
-    setInputvalues(newData);
+    let newdelete = inputValues.filter((data) => data.id !== id);
+    setInputvalues([...newdelete]);
   };
 
   return (
@@ -64,13 +81,13 @@ const Data = () => {
         CRUD OPERATION IN REACT
       </h3>
       <ToastContainer />
-      <div className="w-[80%] mx-auto my-6 bg-slate-900 flex justify-evenly items-start text-white p-2 h-full rounded-md">
+      <div className="w-[90%] mx-auto my-6 bg-slate-900 flex justify-evenly items-start text-white p-2 h-full rounded-md">
         <form
           action=""
           className="form flex justify-between items-center gap-10"
           onSubmit={submitHandler}
         >
-          <div className="p-3">
+          <div className="">
             <label htmlFor="name" className="mr-2 py-2 text-white/70">
               Your Name
             </label>
@@ -78,7 +95,7 @@ const Data = () => {
               type="text"
               placeholder="Add your name..."
               value={name}
-              className="py-2 px-6 rounded-md mt-1 outline-none bg-slate-800   focus:ring-1 focus:ring-white/70 focus:ring-offset-white/90 "
+              className="py-2  px-2 w-full rounded-md mt-1 outline-none bg-slate-800   focus:ring-1 focus:ring-white/70 focus:ring-offset-white/90 "
               onChange={(e) => setName(e.target.value)}
             />
           </div>
@@ -89,7 +106,7 @@ const Data = () => {
               type="email"
               placeholder="Add your email..."
               value={email}
-              className="py-2 px-6 rounded-md mt-1 outline-none bg-slate-800  focus:ring-1 focus:ring-white/70 focus:ring-offset-white/90"
+              className="py-2  px-2 w-full rounded-md mt-1 outline-none bg-slate-800  focus:ring-1 focus:ring-white/70 focus:ring-offset-white/90"
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
@@ -97,55 +114,55 @@ const Data = () => {
             <label htmlFor="task">Add your today's Task</label>
             <input
               type="text"
-              placeholder="Add your email..."
+              placeholder="Add your task..."
               value={task}
-              className="py-2 px-6 rounded-md mt-1  outline-none bg-slate-800  focus:ring-1 focus:ring-white/70 focus:ring-offset-white/90"
+              className="py-2 px-2 w-full rounded-md mt-1  outline-none bg-slate-800  focus:ring-1 focus:ring-white/70 focus:ring-offset-white/90"
               onChange={(e) => setTask(e.target.value)}
             />
           </div>
           <button
             type="submit"
-            className="py-2 px-4 bg-red-500 rounded-md mt-3"
+            className="py-2 px-4 bg-blue-500 rounded-md mt-6 font-semibold"
           >
-            Submit
+            {editid ? "Edit" : "Submit"}
           </button>
         </form>
       </div>
-      {inputValues.length > 0 && (
-        <table className="table w-[80%] mx-auto bg-slate-900 text-white/85 border-collapse border border-slate-500">
+      {inputValues?.length > 0 && (
+        <table className="table w-[83%] mx-auto bg-slate-900 text-white/85 border-collapse border border-slate-500">
           <tbody>
-            <tr className="bg-red-500/80">
+            <tr className="bg-slate-600/80">
               <th className="py-3 border border-slate-600">Name</th>
               <th className="py-3 border border-slate-600">Email</th>
-              <th className="py-3 border border-slate-600">Todo Task</th>
+              <th className="py-3 border border-slate-600">Task</th>
               <th className="py-3 border border-slate-600">Actions</th>
             </tr>
           </tbody>
 
           {inputValues?.map((value, id) => {
             return (
-              <>
+              <Fragment key={id}>
                 <tbody>
-                  <tr className="text-center text-white/70" key={id}>
+                  <tr className="text-center text-white/70">
                     <td className="py-2 border border-slate-600">
                       {value?.name}
                     </td>
                     <td className="py-2 border border-slate-600">
                       {value?.email}
                     </td>
-                    <td className="py-2 border border-slate-600">
+                    <td className=" py-2 border border-slate-600">
                       {value?.task}
                     </td>
                     <td className="py-2 border border-slate-600">
                       <button
-                        onClick={() => handleEdit(value, id)}
+                        onClick={() => handleEdit(value)}
                         className="py-2 px-3 bg-green-500 rounded-md  font-semibold"
                       >
                         {" "}
                         Edit
                       </button>{" "}
                       <button
-                        onClick={() => handleDelete(id)}
+                        onClick={() => handleDelete(value.id)}
                         className="py-2 px-3 bg-red-500 rounded-md ml-2 font-semibold"
                       >
                         Delete
@@ -153,7 +170,7 @@ const Data = () => {
                     </td>
                   </tr>
                 </tbody>
-              </>
+              </Fragment>
             );
           })}
         </table>
